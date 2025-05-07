1fe4b869653a3353cc9eaad507e59e9790fb1273
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2025-05-07 16:03:42 +0200

    Update 03_Population_Distribution.R

--- 03_Population_Distribution.R
+++ 03_Population_Distribution.R
@@ -5,27 +5,22 @@
 # Recall that when we install new packages that are outside of R we need to use the quotes
 # The package sdm is used for species distribution modelling while
 # terra is used to manipulate spatial data and to read files
-
 install.packages("sdm") 
 install.packages("terra")
 
 # To make sure that they are now installed we use library()
-
 library(sdm)
 library(terra) 
 
 # To check file's names inside an R folder we use "system.file()" function
 #Specifying the name of the folder, the name of the file and then the package
-
 system.file("external")
 
 # In the external folder I have many dataset but I want one of them called species.shp
-
 system.file("external/species.shp")  # shp is an extension meaning shape file extension 
 
 
 # Since external is used to be in all folders let's write which package we want the external from:
-
 file <- system.file("external/species.shp", package="sdm")
 
 # The full path in my PC is "C:/Users/anton/AppData/Local/R/win-library/4.4/sdm/external/species.shp"
@@ -35,7 +30,6 @@ file <- system.file("external/species.shp", package="sdm")
 vect(file) 
 
 # As always to make thing clearer we assign the function to an object
-
 rana <- vect(file)
 rana 
 
@@ -44,9 +38,7 @@ rana
 # I know whether in point i, j, n, rana is present or not
 
 # Display the occurence data
-
 rana$Occurrence # always maintain capital letter 
-
 plot(rana) # points representing presence or absence of rana
 
 # We obtaain 0 if the species is absent, 1 if it is present
@@ -58,11 +50,9 @@ plot(rana) # points representing presence or absence of rana
 # To do so we use the previoulsy formed link (rana$occurance) as equal (==) to the
 # elements that we want to extrapolate (1 since in this occurance 1 means presence)
 # To end the input we use ;
-
 pres <- rana[rana$Occurrence==1 ;]
 
 # Exercise: build a multiframe: first image is rana, second image is pres 
-
 par(mfrow=c(1,2))
 plot(rana)
 plot(pres)
@@ -70,24 +60,20 @@ plot(pres)
 # Exercise: select data from rana with only absences
 # Uncertainity is higher for absences due to observer bias (I dont' know if the species is actually
 # missing or if it was just not seen)
-
 abs <- rana[rana$Occurrence==0]
 plot(abs) 
 
 # Exercise: plot in a multiframe presence beside absences 
-
 par(mfrow=c(1,2))
 plot(pres)
 plot(abs)
 
 # Exercise: the same but one above the other 
-
 par(mfrow=c(2,1))
 plot(pres)
 plot(abs)
 
 # Exercise plot presences in blue together with absences in red 
-
 plot(pres, col="blue") # First I open the plot with presence and then 
 points(abs, col="red", pch=19, cex=2) # I use points to highlight the absences in blue
 
@@ -95,7 +81,6 @@ points(abs, col="red", pch=19, cex=2) # I use points to highlight the absences i
 # How to import an image as a file from outside R?
 # We always use the system.file() function but this time we want the file elevation.asc
 # "asc" is a type of file like .jpeg
-
 elev <- system.file("external/elevation.asc", package="sdm") # this time it's not vector file in .shp but a raster file in .asc
 
 # To make it usable to R we won't use vect() but rast()
@@ -103,7 +88,6 @@ elev <- system.file("external/elevation.asc", package="sdm") # this time it's no
 elevmap <- rast(elev)
 
 # Exercise: change colors of the elevamap with the colorRampPalette function
-
 cln<- colorRampPalette(c("purple2", "orange1", "darkmagenta")) (100)
 plot(elevmap, col=cln)
 
