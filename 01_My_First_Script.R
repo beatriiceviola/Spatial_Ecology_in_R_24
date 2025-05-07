60a98f6be0ddaead87ec1c14b80aed3756e75997
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2025-05-07 16:02:16 +0200

    Update 01_My_First_Script.R

--- 01_My_First_Script.R
+++ 01_My_First_Script.R
@@ -2,22 +2,18 @@
 
 # Exercise
 # We will use R as a simple calculator
-
 2 + 3 
 
 # Now we can assign our value to an object by adding the symbol <-
 # In this way we create a new variable with an assigned value
-
 cato <- 2 + 3 
 cato # By plotting "cato" we will diretcly have the result 5
 
 # Let's do another example
-
 beatrice <- 4 + 2
 beatrice 
 
 # Now we can do many more operations by using our new variables
-
 cato + beatrice
 cato ^ beatrice
 (cato + beatrice) ^ cato 
@@ -26,7 +22,6 @@ cato ^ beatrice
 # Arrays or vectors: a series of info coded all together
 # We concatenate all the data by using the "c" function
 # In this way we can create an array, the objects inside are all called arguments
-
 luca <- c(10, 20, 30, 40, 50) 
 luca # Let's run the code
 
@@ -43,19 +38,16 @@ plot(sofia, luca) # By plotting these two variables together we observe a direct
 # To modify the symbols we use "pch=" in the plot's argument
 # To know which one I want to use I can google "symbols in R" 
 # or I can check the site https://www.datanovia.com/en/blog/pch-in-r-best-tips/
-
 plot(sofia, luca, pch=19) 
 
 # Another change that we can make is the dimension of our symbol
 # To do so we use "cex=" in the argument of the plot
-
 plot(sofia, luca, pch=19, cex=2) # In this way we make the fots bigger 
 plot(sofia, luca, pch=19, cex=0.5) # In this way we make them smaller
 
 # Then we can modify the color by using "col=" in the argument
 # To choose which color we either google "colors in R"
 # or check this link https://r-graph-gallery.com/42-colors-names.html
-
 plot(sofia, luca, pch=19, cex=2, col="blue") 
 
 # One last change that I can m,ake is to add the labels in the plot
