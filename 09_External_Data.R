6fd81810af34c254f590859e9d9ae75423b1b5ed
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2025-05-07 16:38:32 +0200

    Update 09_External_Data.R

--- 09_External_Data.R
+++ 09_External_Data.R
@@ -1,4 +1,4 @@
-#Importing data from external sources da finire di correggere
+#Importing data from external sources 
 
 # First let's recall the packages
 library(terra)
@@ -9,22 +9,34 @@ library(terra)
 # setwd means "set working directory"
 # The argument of this function, that has to be always between the quotes since we get out of R,
 # is the path of the folder
-setwd("/Users/macbookairair/Downloads/Cartella 1/1.png")
-getwd()
+setwd("/Users/macbookairair/Downloads/Cartella")
+# The path to the folder will depend on the OS of your computer
+# Note that in windows, the path you get from windows explorer has opposite slashes that you need to change
+
 
 # In the package terra there is the function rast() that allow us to import data
-scotland <- rast("scotland_outerhebrides_oli_20240918_lrg.jpg")
-plotRGB(scotland, r=1, g=2, b=3) # Let's use the natural colors
+emi1 <- rast("1.jpg")
 
-# alternative function to plot for library terra
-plot(scotland)
+# Now let's do a RGB plot assigning r, g and b according your image bands
+plotRGB(emi1, r=1, g=2, b=3) # Let's use the natural colors
 
+# Exercise: Download a second image from the same site and import it in R
+# After downloading the file, load the file using "rast" function
+emi2 <- rast("2.jpg")
+plotRGB(emi2, r=1, g=2, b=3) # RGB plot in natural colors
 
-# Exercise: dowload an image from Earth Observatory and upload it in R
-Patagonian_Shelf_Waters_Abloom <- rast("bloom_pace_20241130_lrg.jpg")
-plot(Patagonian_Shelf_Waters_Abloom)
+# Create a multi-frame plot to compare the two images
+par(mfrow=c(2,1)) # 2 rows and 1 column
+plotRGB(emi1, r=1, g=2, b=3) # First image
+plotRGB(emi2, r=1, g=2, b=3) # Second image
 
+# Exercise: Multitemporal change detection
+emidif = emi1[[1]] - emi2[[1]] # Calculate the difference between the 2 images on the first band
+cl <- colorRampPalette(c("lightgreen", "darkmagenta", "orange")) (100) # Create a new color palette for plotting the difference
+plot(emidif, col=cl) # Plot the difference with the new color palette
 
-# for Windows users: 
-# working directory works with / not \
-# you should add manually the extension of the image (eg: .jpg)
+# The Mato Grosso image can be downloaded directly from the NASA Earth Observatory website
+mato <- rast("matogrosso_l5_1992219_lrg.jpg") # Load the image
+# Plot the different RGB band combinations
+plotRGB(mato, r=1, g=2, b=3) 
+plotRGB(mato, r=2, g=1, b=3) 
