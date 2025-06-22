b13df0c55252b66b8254c9ffd151bc3a02df78b1
beatriiceviola <162352305+beatriiceviola@users.noreply.github.com>
2025-06-22 19:18:48 +0200

    Update 00_Exam_Project.R

--- 00_Exam_Project.R
+++ 00_Exam_Project.R
@@ -1,15 +1,26 @@
+## Wildfires in Manitoba, Canada
+# This projects aims to value the area damaged by fire during the month of May in 2025 in the region of Manitoba, Canada.
+# The data were taken from Copernicus Browser 
 
-#Import libraries
-library(terra) #to use satellite images
-library(imageRy) #for manipulating raster images in R
-library(viridis) #for color palettes
-library(ggplot2) #to create graphs
-library(patchwork) #for coupling ggplot2 graphs
+# First let's import the libraries that we will need:
+library(terra) # For Spatial Analysis
+library(imageRy) # To manipulate and share raster images in R
+library(viridis) # For color palettes that are visible also for color blin people
+library(ggplot2) # To create graphics
+library(patchwork) # For coupling graphics
 
-#devtools:: install_github("ducciorocchini/imageRy")
+# Let's specify how I installed the package "imageRy"
+devtools:: install_github("ducciorocchini/imageRy")
 
+# Then I set the working directory on R, this is the folder from which R will take all the datas
 setwd("/Users/macbookairair/Downloads/Cartella")
 
+# Loading the images
+# The first images were taken by Sentinel-2 and are in true color
+# All the bands were already decided by decided and are disposed in this way:
+# First band: R = red
+# Second band: G = green
+# Third band: B = blue
 suppressWarnings({
 tc_may4 <- rast("begmay.jpeg")
 })
@@ -20,6 +31,8 @@ suppressWarnings({
 tc_june12 <- rast("june.jpeg")
 })
 
+# Then I wanted to visualize these images all together so, thanks to the par(mfrow=) function
+# I arranged them in one row and three columns
 par(mfrow=c(1,3))
 plot(tc_may4)
 title("May 4", line = -6, cex=2) 
@@ -29,6 +42,11 @@ plot(tc_june12)
 title("June 12", line = -6, cex=2) 
 dev.off()
 
+# Loading the images
+# Other important datasets to import were the "Swir" images
+# First band: R = SWIR
+# Second Band: G = NIR
+# Third Band: B = red
 suppressWarnings({
 swir_may4 <- rast("begmay_swir.jpeg")
 })
@@ -39,6 +57,7 @@ suppressWarnings({
 swir_june12 <- rast("june_swir.jpeg")
 })
 
+# And then again put them together to visualize them alligned
 par(mfrow=c(1,3))
 plot(swir_may4)
 title("May 4", line = -6, cex=2) 
@@ -155,3 +174,38 @@ prop2
 #Burned areas= 14.3%
 perc2 = prop2*100
 perc2
+
+suppressWarnings({
+fc2017 <- rast("fc2017.jpeg")
+})
+suppressWarnings({
+fc2024 <- rast("fc2024.jpeg")
+})
+
+class2017 <- im.classify(fc2017, num_clusters=3)
+class.names <- c("Damaged vegetation", "Healthy vegetation", "Water Bodies")
+plot(class2017, main= "Classification 2017", type="classes", levels=class.names, col= c("darkkhaki","darkolivegreen","darkblue"))
+
+class2024 <- im.classify(fc2024, num_clusters=3)
+class.names <- c("Damaged vegetation", "Healthy vegetation", "Water Bodies")
+plot(class2024, main= "Classification 2024", type="classes", levels=class.names, col= c("darkkhaki","darkolivegreen","darkblue"))
+
+#Percentages 2017: 
+# Damaged vegetation = 4.9%
+# Healthy vegetation = 89.5%
+# Water bodies = 5.6%
+freq17 <- freq(class2017)
+tot17 <- ncell (class2017)
+prop17 = freq17/tot17
+perc17 = prop17*100
+perc17
+
+#Percentages 2024: 
+# Damaged vegetation = 15.5%
+# Healthy vegetation = 79.1%
+# Water bodies = 5.4%
+freq24 <- freq(class2024)
+tot24 <- ncell (class2024)
+prop24 = freq24/tot24
+perc24 = prop24*100
+perc24
